378b5cccc2515340705c32de44c5792ed6f0dbfc
Bach Le <bach@bullno1.com>
2026-01-16 15:11:43 +0800

    Start object state sync implementation

--- src/slopsync.c
+++ src/slopsync.c
@@ -1,34 +1,97 @@
 #include "slopsync.h"
+#include <slopnet.h>
 #include <bent.h>
 #include <bgame/allocator/frame.h>
 #include <blog.h>
 #include <string.h>
 #include <errno.h>
-#include "chibihash64.h"
+#include <inttypes.h>
+#include <bhash.h>
 #include "ecs.h"
+
+BENT_DEFINE_POD_COMP(comp_slopsync_link, slopsync_link_t)
+
+BENT_DECLARE_SYS(sys_slopsync)
 
 typedef struct {
 	ssync_t* ssync;
+	bent_world_t* world;
+
+	BHASH_TABLE(ssync_net_id_t, bent_t) net_to_local;
 } sys_slopsync_t;
 
-BENT_DEFINE_SYS(slopsync) = {
-	.size = sizeof(sys_slopsync_t),
-};
-
 static bent_t
-ssync_translate_id(sys_slopsync_t* sys, ssync_net_id_t net_id) {
-	return (bent_t){ 0 };
+ssync_net_to_local(sys_slopsync_t* sys, ssync_net_id_t net_id) {
+	bhash_index_t index = bhash_find(&sys->net_to_local, net_id);
+	if (bhash_is_valid(index)) {
+		return sys->net_to_local.values[index];
+	} else {
+		return (bent_t){ 0 };
+	}
+}
+
+static void*
+sys_ssync_realloc(
+	void* userdata,
+	void* ptr,
+	size_t size
+) {
+	sys_slopsync_t* sys = userdata;
+	return bgame_realloc(ptr, size, bent_memctx(sys->world));
 }
 
 static void
-ssync_bent_sync(
+sys_ssync_create(void* userdata, ssync_net_id_t net_id) {
+	sys_slopsync_t* sys = userdata;
+
+	bent_t local = bent_create(sys->world);
+	bent_add_comp_slopsync_link(sys->world, local, &(slopsync_link_t){ .id = net_id });
+}
+
+static void
+sys_ssync_destroy(void* userdata, ssync_net_id_t net_id) {
+	sys_slopsync_t* sys = userdata;
+	bent_destroy(sys->world, ssync_net_to_local(sys, net_id));
+}
+
+static void
+sys_ssync_add_prop_group(
+	void* userdata,
+	ssync_net_id_t net_id, ssync_local_id_t prop_group_id
+) {
+	sys_slopsync_t* sys = userdata;
+	bent_t local = ssync_net_to_local(sys, net_id);
+	bent_add(sys->world, local, (bent_comp_reg_t){ .id = prop_group_id }, NULL);
+}
+
+static void
+sys_ssync_rem_prop_group(
+	void* userdata,
+	ssync_net_id_t net_id, ssync_local_id_t prop_group_id
+) {
+	sys_slopsync_t* sys = userdata;
+	bent_t local = ssync_net_to_local(sys, net_id);
+	bent_remove(sys->world, local, (bent_comp_reg_t){ .id = prop_group_id });
+}
+
+static bool
+sys_ssync_has_prop_group(
+	void* userdata,
+	ssync_net_id_t obj_id,
+	ssync_local_id_t prop_group_id
+) {
+	return false;
+}
+
+static void
+sys_ssync_sync(
 	void* userdata,
 	ssync_ctx_t* ctx,
 	ssync_net_id_t obj_id
 ) {
 	bent_world_t* world = userdata;
-	sys_slopsync_t* sys = bent_get_sys_data(world, slopsync);
-	bent_t ent = ssync_translate_id(sys, obj_id);
+	sys_slopsync_t* sys = bent_get_sys_data(world, sys_slopsync);
+	bent_t ent = ssync_net_to_local(sys, obj_id);
 
 	if (ssync_prop_group(ctx, comp_transform.id)) {
 		transform_t* transform = bent_get_comp_transform(world, ent);
@@ -39,22 +102,96 @@ ssync_bent_sync(
 	}
 }
 
-size_t
-ssync_bent_schema_size(bent_world_t* world) {
-	return ssync_schema_size(ssync_bent_sync, world);
+static void
+sys_ssync_init(void* userdata, bent_world_t* world) {
+	sys_slopsync_t* sys = userdata;
+	sys->world = world;
+
+	bhash_config_t hconfig = bhash_config_default();
+	hconfig.memctx = bent_memctx(world);
+	bhash_reinit(&sys->net_to_local, hconfig);
+
+	ssync_config_t ssync_config = {
+		.max_message_size = snet_max_message_size(),
+		.realloc = sys_ssync_realloc,
+		.create_obj = sys_ssync_create,
+		.destroy_obj = sys_ssync_destroy,
+		.add_prop_group = sys_ssync_add_prop_group,
+		.rem_prop_group = sys_ssync_rem_prop_group,
+		.has_prop_group = sys_ssync_has_prop_group,
+		.sync = sys_ssync_sync,
+
+		.userdata = sys,
+	};
+	ssync_reinit(&sys->ssync, &ssync_config);
 }
 
-void
-ssync_bent_write_schema(bent_world_t* world, void* out, size_t out_size) {
-	ssync_write_schema(ssync_bent_sync, world, out, out_size);
+static void
+sys_ssync_cleanup(void* userdata, bent_world_t* world) {
+	sys_slopsync_t* sys = userdata;
+
+	ssync_cleanup(sys->ssync);
+	bhash_cleanup(&sys->net_to_local);
 }
+
+static void
+sys_ssync_add(void* userdata, bent_world_t* world, bent_t entity) {
+	sys_slopsync_t* sys = userdata;
+	slopsync_link_t* link = bent_get_comp_slopsync_link(world, entity);
+
+	if (link->id.bin == 0) {
+		link->id = ssync_create(sys->ssync, link->flags);
+	}
+
+	bhash_put(&sys->net_to_local, link->id, entity);
+}
+
+static void
+sys_ssync_remove(void* userdata, bent_world_t* world, bent_t entity) {
+	sys_slopsync_t* sys = userdata;
+	slopsync_link_t* link = bent_get_comp_slopsync_link(world, entity);
+
+	const ssync_obj_info_t* obj_info = ssync_obj_info(sys->ssync, link->id);
+	if (obj_info && obj_info->is_local) {
+		ssync_destroy(sys->ssync, link->id);
+	}
+
+	bhash_remove(&sys->net_to_local, link->id);
+}
+
+static void
+sys_ssync_update(
+	void* userdata,
+	bent_world_t* world,
+	bent_mask_t update_mask,
+	bent_t* entities,
+	bent_index_t num_entities
+) {
+	sys_slopsync_t* sys = userdata;
+
+	if (update_mask == UPDATE_MASK_VAR_PRE) {
+		ssync_update(sys->ssync, CF_DELTA_TIME);
+	}
+}
+
+BENT_DEFINE_SYS(sys_slopsync) = {
+	.size = sizeof(sys_slopsync_t),
+	.init = sys_ssync_init,
+	.cleanup = sys_ssync_cleanup,
+	.allow_reinit = true,
+	.require = BENT_COMP_LIST(&comp_slopsync_link),
+	.add = sys_ssync_add,
+	.remove = sys_ssync_remove,
+	.update_mask = UPDATE_MASK_VAR_PRE,
+	.update = sys_ssync_update,
+};
 
 void
 ssync_bent_sync_static_schema(bent_world_t* world, const ssync_static_schema_t* schema) {
-	size_t size = ssync_schema_size(ssync_bent_sync, world);
+	size_t size = ssync_schema_size(sys_ssync_sync, world);
 	uint8_t* content = bgame_alloc_for_frame(size, _Alignof(uint8_t));
-	ssync_write_schema(ssync_bent_sync, world, content, size);
-	uint64_t hash = chibihash64(content, size, 0);
+	ssync_write_schema(sys_ssync_sync, world, content, size);
+	uint64_t hash = bhash_hash(content, size);
 
 	if (size == schema->size && hash == schema->hash) {
 		return;
@@ -86,7 +223,7 @@ ssync_bent_sync_static_schema(bent_world_t* world, const ssync_static_schema_t* 
 	fprintf(file, "static const ssync_static_schema_t ssync_schema = {\n");
 	fprintf(file, "\t.filename = __FILE__,\n");
 	fprintf(file, "\t.size = %zuull,\n", size);
-	fprintf(file, "\t.hash = %zuull,\n", hash);
+	fprintf(file, "\t.hash = %" PRIu64 "ull,\n", hash);
 	fprintf(file, "\t.content = ssync_schema_content,\n");
 	fprintf(file, "};\n");
 
--- src/slopsync.h
+++ src/slopsync.h
@@ -11,7 +11,45 @@ typedef struct {
 	const uint8_t* content;
 } ssync_static_schema_t;
 
+typedef struct {
+	ssync_net_id_t id;
+	ssync_obj_flags_t flags;
+} slopsync_link_t;
+
+BENT_DECLARE_COMP(comp_slopsync_link)
+BENT_DEFINE_COMP_ADDER(comp_slopsync_link, slopsync_link_t)
+BENT_DEFINE_COMP_GETTER(comp_slopsync_link, slopsync_link_t)
+
+typedef void (*ssync_control_callback_fn_t)(
+	void* userdata,
+	bent_world_t* world,
+	ssync_player_id_t sender,
+	bent_t entity,
+	ssync_blob_t command
+);
+
+void
+ssync_register_control_callback(
+	bent_world_t* world,
+	bent_sys_reg_t listener,
+	ssync_control_callback_fn_t callback
+);
+
 void
 ssync_bent_sync_static_schema(bent_world_t* world, const ssync_static_schema_t* schema);
+
+const ssync_t*
+ssync_bent_client(bent_world_t* world);
+
+const ssync_info_t*
+ssync_bent_info(bent_world_t* world);
+
+const ssync_obj_info_t*
+ssync_bent_entity_info(bent_world_t* world, bent_t ent);
+
+static inline void
+ssync_bent_track(bent_world_t* world, bent_t ent, ssync_obj_flags_t flags) {
+	bent_add_comp_slopsync_link(world, ent, &(slopsync_link_t){ .flags = flags });
+}
 
 #endif
